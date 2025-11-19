5abbb74c5a7dddf7359bb78b0eee8d2f1b5446c2
meisdyxd <kararturkar@gmail.com>
2025-11-20 00:12:32 +0300

    feat: add authentication service

---
+++ src/backend/AuthenticationService/AuthenticationService/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ILogger<AuthenticationController> _logger;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
