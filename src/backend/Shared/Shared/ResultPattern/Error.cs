7981c7b32a6dc01cd0bb2b6f1c72d175dd3222f9
meisdyxd <kararturkar@gmail.com>
2025-11-20 00:13:26 +0300

    feat: add shared project

---
+++ src/backend/Shared/Shared/ResultPattern/Error.cs
@@ -0,0 +1,18 @@
+﻿namespace Shared.ResultPattern;
+
+public sealed record Error
+{
+    public Error(
+        string reason,
+        string action,
+        string code)
+    {
+        Reason = reason; 
+        Action = action;
+        Code = code;
+    }
+
+    public string Reason { get; set; }
+    public string Action { get; set; }
+    public string Code { get; set; }
+}
