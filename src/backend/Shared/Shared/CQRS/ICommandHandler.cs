509f74477206cfbcad4969e834fb0241120fda6a
meisdyxd <kararturkar@gmail.com>
2025-11-21 23:19:06 +0300

    feat: add cqrs

---
+++ src/backend/Shared/Shared/CQRS/ICommandHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.CQRS;
+
+public interface ICommandHandler<TCommand, TResult>
+    where TCommand : ICommand<TResult>
+{
+    Task<TResult> Handle(TCommand command, CancellationToken cancellationToken = default);
+}
+
+public interface ICommandHandler<TCommand>
+    where TCommand : ICommand
+{
+    Task Handle(TCommand command, CancellationToken cancellationToken = default);
+}
---
+++ src/backend/Shared/Shared/Extensions/ValidationExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Shared.Extensions;
+
+public static class ValidationExtensions
+{
+    public static T ThrowIfNull<T>(this T value, string paramName)
+        where T : class
+        => value ?? throw new ArgumentNullException(paramName);
+
+    public static T ThrowIfDefault<T>(this T value, string paramName)
+        where T : struct, IEquatable<T>
+        => value.Equals(default)
+            ? throw new ArgumentException($"{paramName} cannot be default value", paramName)
+            : value;
+
+    public static string ThrowIfNullOrWhiteSpace(this string? value, string paramName)
+        => string.IsNullOrWhiteSpace(value)
+            ? throw new ArgumentException($"{paramName} cannot be null or whitespace", paramName)
+            : value;
+
+    public static string ThrowIfNullOrEmpty(this string? value, string paramName)
+        => string.IsNullOrEmpty(value)
+            ? throw new ArgumentException($"{paramName} cannot be null or empty", paramName)
+            : value;
+}
