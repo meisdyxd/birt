a50a678570ba91253c7e41c68bd0a1d9c524e6cc
meisdyxd <kararturkar@gmail.com>
2025-11-24 22:49:50 +0300

    authz: migrate to clean arch.

---
+++ src/backend/AuthorizationService/AuthorizationService.Infrastructure/DependencyInjections.cs
@@ -0,0 +1,21 @@
+﻿using AuthorizationService.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Extensions;
+
+namespace AuthorizationService.Infrastructure;
+
+public static class DependencyInjections
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AuthzDbContext>(act =>
+        {
+            var connectionString = configuration.GetConnectionString("Postgres").ThrowIfNullOrWhiteSpace("Authz Postgres");
+            act.UseNpgsql(connectionString);
+        });
+
+        return services;
+    }
+}
